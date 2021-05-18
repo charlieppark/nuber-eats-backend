a4a06fd66945274431a0bc159c341d390f93967c
CharliePark <chanho0309@gmail.com>
2021-05-18 21:15:04 +0900

    ~lec 2.0

---
+++ src/restaurants/entities/restaurant.entity.ts
@@ -0,0 +1,18 @@
+import { Field, ObjectType } from "@nestjs/graphql";
+
+
+@ObjectType()
+export class Restaurant {
+
+    @Field(is => String)
+    name: string;
+
+    @Field(type => Boolean, {nullable:true})
+    isVegan? : boolean;
+
+    @Field(type => String)
+    address: string;
+
+    @Field(type => String)
+    ownersName : string;
+} 
